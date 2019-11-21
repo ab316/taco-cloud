1ad4c7f27bf6bd04c28ea78a0c4840bd82b9e5cd
Abdullah Baig <abdullahbaig456@gmail.com>
2019-11-21 20:51:25 +0100

    Add validation in design and order

--- src/main/java/com/learning/tacos/controller/DesignTacoController.java
+++ src/main/java/com/learning/tacos/controller/DesignTacoController.java
@@ -6,10 +6,13 @@ import com.learning.tacos.model.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,6 +24,21 @@ public class DesignTacoController {
 
     @GetMapping
     public String showDesignForm(Model model) {
+        model.addAttribute("design", new Taco());
+        return "design";
+    }
+
+    @PostMapping
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+        log.info("Processing design: {}", design);
+        return "redirect:/orders/current";
+    }
+
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
         List<Ingredient> ingredients = Arrays.asList(
                 new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
                 new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
@@ -34,18 +52,9 @@ public class DesignTacoController {
                 new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
         );
 
-        for (var type : Ingredient.Type.values()) {
+        for (var type : Type.values()) {
             model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
         }
-
-        model.addAttribute("design", new Taco());
-        return "design";
-    }
-
-    @PostMapping
-    public String processDesign(Taco design) {
-        log.info("Processing design: {}", design);
-        return "redirect:/orders/current";
     }
 
     private List<Ingredient> filterByType(List <Ingredient> ingredients, Type type) {
